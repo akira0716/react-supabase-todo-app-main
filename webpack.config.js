e07131e0757d82990e13702224f481e2481ff9e0
akira0716 <akira.inaji.0716@gmail.com>
2024-01-20 00:59:48 +0900

    あ

--- webpack.config.js
+++ webpack.config.js
@@ -38,18 +38,6 @@ module.exports = {
         test: /\.css$/,
         use: ["style-loader", "css-loader"],
       },
-      {
-        test: /\.(png|jpe?g|gif|svg)$/i,
-        use: [
-          {
-            loader: "file-loader",
-            options: {
-              name: "[name].[ext]",
-              outputPath: OUTPUT_PATH, // 画像を出力するディレクトリ
-            },
-          },
-        ],
-      },
     ],
   },
   resolve: {
