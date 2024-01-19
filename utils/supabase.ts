3c2d4280cdf59dd1457e5c5871708a2b5890420b
akira0716 <akira.inaji.0716@gmail.com>
2024-01-19 23:05:17 +0900

    後でenvの変数に戻して！

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,7 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.REACT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.REACT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = "https://yrlpjrhizgzfvcfewccy.supabase.co";
+const supabaseAnonKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlybHBqcmhpemd6ZnZjZmV3Y2N5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDU2Njk5NTEsImV4cCI6MjAyMTI0NTk1MX0.15oyD8TdAkpWmEHSTR6pN7u45fJ95DH1YdzShpyjgc8";
 
 // Create a single supabase client for interacting with your database
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
