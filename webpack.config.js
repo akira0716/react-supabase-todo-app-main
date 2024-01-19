75f3b3b2e753dd723ba61140e0c73563ec81ea12
akira0716 <akira.inaji.0716@gmail.com>
2024-01-20 01:37:04 +0900

    pppp

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const Dotenv = require("dotenv-webpack");
 const workBoxWebpackPlugin = require("workbox-webpack-plugin");
 const WebpackPwaManifest = require("webpack-pwa-manifest");
 const OUTPUT_PATH = `${__dirname}/dist`;
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -49,25 +50,28 @@ module.exports = {
       template: __dirname + "/src/index.html",
     }),
     new Dotenv(),
-    new WebpackPwaManifest({
-      short_name: "short name", // ホーム画面のラベルに表示される名称
-      name: "app full name", // appの名前
-      display: "standalone", // standaloneにするとブラウザのUI要素が削除されてアプリっぽくなる
-      start_url: "index.html", // 開始時に起動するページ
-      short_name: "3D Timer",
-      name: "3D Ramen Timer",
-      background_color: "#202124",
-      theme_color: "#FF0000",
-      icons: [
-        {
-          sizes: "192x192",
-          src: "./src/images/R5.png",
-          type: "image/png",
-        },
-      ],
-    }),
+    // new WebpackPwaManifest({
+    //   short_name: "short name", // ホーム画面のラベルに表示される名称
+    //   name: "app full name", // appの名前
+    //   display: "standalone", // standaloneにするとブラウザのUI要素が削除されてアプリっぽくなる
+    //   start_url: "index.html", // 開始時に起動するページ
+    //   short_name: "3D Timer",
+    //   name: "3D Ramen Timer",
+    //   background_color: "#202124",
+    //   theme_color: "#FF0000",
+    //   icons: [
+    //     {
+    //       sizes: "192x192",
+    //       src: "./src/images/R5.png",
+    //       type: "image/png",
+    //     },
+    //   ],
+    // }),
     new workBoxWebpackPlugin.GenerateSW({
       swDest: OUTPUT_PATH + "/service-worker.js",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{ from: "public", to: "." }],
     }),
   ],
   devServer: {
