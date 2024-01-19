b2068ed3e7c5018cb405b5b2f487bcd207a1f4aa
akira0716 <akira.inaji.0716@gmail.com>
2024-01-19 23:56:00 +0900

    PWA化

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
-import React from 'react';
-import {createRoot} from 'react-dom/client';
-import App from './App';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
 
-const rootElem = document.getElementById('root');
-if(rootElem){
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("./service-worker.js");
+  });
+}
+
+const rootElem = document.getElementById("root");
+if (rootElem) {
   const root = createRoot(rootElem);
   root.render(<App />);
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,17 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const Dotenv = require('dotenv-webpack');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
+const workBoxWebpackPlugin = require("workbox-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const OUTPUT_PATH = `${__dirname}/dist`;
 
 module.exports = {
-  mode: 'development',
+  mode: "development",
   entry: {
-    main: __dirname + '/src/main.tsx',
+    main: __dirname + "/src/main.tsx",
   },
   output: {
-    path: __dirname + '/dist',
-    filename: '[name].js',
+    path: __dirname + "/dist",
+    filename: "[name].js",
   },
   module: {
     rules: [
@@ -17,39 +20,48 @@ module.exports = {
         exclude: /node_modules/,
         use: [
           {
-            loader: 'babel-loader',
+            loader: "babel-loader",
             options: {
               presets: [
-                '@babel/preset-env',
-                '@babel/preset-react',
-                '@babel/preset-typescript',
+                "@babel/preset-env",
+                "@babel/preset-react",
+                "@babel/preset-typescript",
               ],
             },
           },
           {
-            loader: 'ts-loader',
+            loader: "ts-loader",
           },
         ],
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
       },
     ],
   },
   resolve: {
-    modules: [__dirname + '/node_modules'],
-    extensions: ['.ts', '.tsx', '.js'],
+    modules: [__dirname + "/node_modules"],
+    extensions: [".ts", ".tsx", ".js"],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: __dirname + '/src/index.html',
+      template: __dirname + "/src/index.html",
     }),
     new Dotenv(),
+    new WebpackPwaManifest({
+      short_name: "short name", // ホーム画面のラベルに表示される名称
+      name: "app full name", // appの名前
+      display: "standalone", // standaloneにするとブラウザのUI要素が削除されてアプリっぽくなる
+      start_url: "index.html", // 開始時に起動するページ
+    }),
+    new workBoxWebpackPlugin.GenerateSW({
+      swDest: OUTPUT_PATH + "/service-worker.js",
+    }),
   ],
   devServer: {
     static: {
-      directory: __dirname + '/dist',
+      directory: __dirname + "/dist",
     },
     port: 8080,
   },
