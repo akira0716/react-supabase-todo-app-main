e6cdca7b8c2f853d3b34ba159338664be36175a6
akira0716 <akira.inaji.0716@gmail.com>
2024-01-21 19:07:02 +0900

    ds

--- webpack.config.js
+++ webpack.config.js
@@ -50,23 +50,9 @@ module.exports = {
       template: __dirname + "/src/index.html",
     }),
     new Dotenv(),
-    // new WebpackPwaManifest({
-    //   short_name: "short name", // ホーム画面のラベルに表示される名称
-    //   name: "app full name", // appの名前
-    //   display: "standalone", // standaloneにするとブラウザのUI要素が削除されてアプリっぽくなる
-    //   start_url: "index.html", // 開始時に起動するページ
-    //   short_name: "3D Timer",
-    //   name: "3D Ramen Timer",
-    //   background_color: "#202124",
-    //   theme_color: "#FF0000",
-    //   icons: [
-    //     {
-    //       sizes: "192x192",
-    //       src: "./src/images/R5.png",
-    //       type: "image/png",
-    //     },
-    //   ],
-    // }),
+    new (require("webpack").DefinePlugin)({
+      "process.env": JSON.stringify(process.env),
+    }),
     new workBoxWebpackPlugin.GenerateSW({
       swDest: OUTPUT_PATH + "/service-worker.js",
     }),
