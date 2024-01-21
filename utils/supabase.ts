7529a76ae8c532bebc8e329c3c792b6279d91d16
akira0716 <akira.inaji.0716@gmail.com>
2024-01-21 17:23:04 +0900

    環境変数をString型にキャスト

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -2,6 +2,6 @@ import { createClient } from "@supabase/supabase-js";
 
 // Create a single supabase client for interacting with your database
 export const supabase = createClient(
-  process.env.EXPO_PUBLIC_SUPABASE_URL!,
-  process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!
+  String(process.env.EXPO_PUBLIC_SUPABASE_URL),
+  String(process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY)
 );
