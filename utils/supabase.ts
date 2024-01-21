124c03b31ef8177122000db657c92b4b8e74d966
akira0716 <akira.inaji.0716@gmail.com>
2024-01-21 18:19:55 +0900

    ddddd

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,21 +1,18 @@
-"use server";
-
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.REACT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.REACT_PUBLIC_SUPABASE_ANON_KEY;
+export const supabase = async () => {
+  "use server";
 
-console.log(supabaseUrl);
-console.log(supabaseKey);
+  const supabaseUrl = process.env.REACT_PUBLIC_SUPABASE_URL;
+  const supabaseKey = process.env.REACT_PUBLIC_SUPABASE_ANON_KEY;
 
-if (!supabaseUrl) {
-  throw new Error("URLがないよ");
-}
+  if (!supabaseUrl) {
+    throw new Error("URLがないよ");
+  }
 
-if (!supabaseKey) {
-  throw new Error("KEYがないよ");
-}
+  if (!supabaseKey) {
+    throw new Error("KEYがないよ");
+  }
 
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-export default supabase;
+  return createClient(supabaseUrl, supabaseKey);
+};
