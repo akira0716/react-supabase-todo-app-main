c8579b1dacba6f12da8fe00fa0ee424cd10d1653
akira0716 <akira.inaji.0716@gmail.com>
2024-01-21 17:42:11 +0900

    c

--- utils/supabaseFunctions.ts
+++ utils/supabaseFunctions.ts
@@ -1,4 +1,4 @@
-import { supabase } from "./supabase";
+import supabase from "./supabase";
 
 export const fetchTodoList = async () => {
   const todoItems = await supabase.from("todo_items").select("*");
