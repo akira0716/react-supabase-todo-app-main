0d3fc4832b5a33aaa2d79300207971d270ada31e
akira0716 <akira.inaji.0716@gmail.com>
2024-01-21 16:41:20 +0900

    VERCELで環境変数が使用できない問題の対応(仮1)

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,8 +1,19 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://yrlpjrhizgzfvcfewccy.supabase.co";
-const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlybHBqcmhpemd6ZnZjZmV3Y2N5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDU2Njk5NTEsImV4cCI6MjAyMTI0NTk1MX0.15oyD8TdAkpWmEHSTR6pN7u45fJ95DH1YdzShpyjgc8";
-
 // Create a single supabase client for interacting with your database
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+let supabaseUrl;
+let supabaseAnonKey;
+let method;
+
+if (
+  process.env.REACT_PUBLIC_SUPABASE_URL &&
+  process.env.REACT_PUBLIC_SUPABASE_ANON_KEY
+) {
+  supabaseUrl = process.env.REACT_PUBLIC_SUPABASE_URL;
+  supabaseAnonKey = process.env.REACT_PUBLIC_SUPABASE_ANON_KEY;
+
+  method = createClient(supabaseUrl, supabaseAnonKey);
+}
+
+export const supabase = method;
