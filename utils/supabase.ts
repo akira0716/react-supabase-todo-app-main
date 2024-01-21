deccfdd3eaa25546049b00e924c9fcfeddeb212e
akira0716 <akira.inaji.0716@gmail.com>
2024-01-21 17:26:12 +0900

    ｓ

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -2,6 +2,6 @@ import { createClient } from "@supabase/supabase-js";
 
 // Create a single supabase client for interacting with your database
 export const supabase = createClient(
-  String(process.env.EXPO_PUBLIC_SUPABASE_URL),
-  String(process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY)
+  String(process.env.REACT_PUBLIC_SUPABASE_URL),
+  String(process.env.REACT_PUBLIC_SUPABASE_ANON_KEY)
 );
